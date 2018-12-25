b92f5fb3e9f99619dd98f5fb8b76faa9b7318040
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2018-12-25 19:36:10 +0530

    Corrected conv1dglu

--- Modules/Conv1dGLU.py
+++ Modules/Conv1dGLU.py
@@ -7,30 +7,25 @@ import sys
 
 class Conv1dGLU(nn.Module):
     '''
-    Implementation of the Conv1d + GLU(Gated Linear Unit)
-    with residual connection.
-    For GLU refer to https://arxiv.org/abs/1612.08083 paper.
-    '''
+        Implementation of the Conv1d + GLU(Gated Linear Unit)
+        with residual connection.
+        For GLU refer to https://arxiv.org/abs/1612.08083 paper.
+        '''
     def __init__(self, in_channels=128, out_channels=128,padding = None,
-                dilation = 2,kernel_size=12,*args, **kwargs):
+                 dilation = 2,kernel_size=12,*args, **kwargs):
         super(Conv1dGLU, self).__init__()
         if padding == None:
             padding = int(((kernel_size-1)/2)*dilation)
-        self.conv1 = nn.Conv1d(in_channels, out_channels=out_channels,
-                                padding=padding,dilation = dilation,
-                                kernel_size=kernel_size)
-        self.conv2 = nn.Conv1d(in_channels, out_channels = out_channels,
-                                padding=padding,dilation = dilation,
-                                kernel_size=kernel_size)
+        self.conv1 = nn.Conv1d(in_channels, out_channels=2 * out_channels,
+                               padding=padding, dilation = dilation,
+                               kernel_size=kernel_size)
+            
     def forward(self, x):
         residual = x
-        # print(x.size())
-        x1 = self.conv1(x)
-        # print(x1.shape)
-        x2 = self.conv2(x)
-        # print(x2.shape)
-        # GLU
+        x = self.conv1(x)
+        x1, x2 = torch.split(x, split_size_or_sections = 128, dim = 1)
         x = x1 * torch.sigmoid(x2)
         x += residual
         x *= math.sqrt(0.5)
         return x
+
